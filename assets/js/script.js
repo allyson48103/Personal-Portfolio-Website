252a14e3b72e64dc2ed050fd8fbe2676dadbe1aa
Allyson Franklin <145939580+allyson48103@users.noreply.github.com>
2024-12-05 16:57:10 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,10 +230,6 @@ srtop.reveal('.education .box', { interval: 200 });
 /* SCROLL PROJECTS */
 srtop.reveal('.work .box', { interval: 200 });
 
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
-
 /* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
